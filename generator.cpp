6282892944cd8bae83e9f9885123c3249b7b13a3
Eduardo Soares <soaresdigo398@gmail.com>
2025-02-15 11:44:40 +0000

    esfera feita

--- generator.cpp
+++ generator.cpp
@@ -7,8 +7,13 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
 //0- 0.5*length, 0, 0.5*length ----->>>>
 void plane(float length,int divisions, char* file){
+	//nrvertices
+	//spamar os vertices todos
+	//comprimento do lado
+	//nr divisões
 	FILE *fd = fopen(file,"w+");
 	int vertices = divisions * divisions * 2 * 3;
 	fprintf(fd,"%d\n",vertices);
@@ -20,7 +25,6 @@ void plane(float length,int divisions, char* file){
 		ponto1[2] = 0 + 0.5*length-espacamento*i;
 		for(int j=0;j<divisions;j++){
 			fprintf(fd, "%f %f %f\n",ponto1[0],ponto1[1], ponto1[2]);
-			fprintf(fd, "%f %f %f\n",ponto1[0],ponto1[1],ponto1[2]);
 			fprintf(fd, "%f %f %f\n",ponto1[0]+espacamento,ponto1[1],ponto1[2]);
 			fprintf(fd, "%f %f %f\n",ponto1[0]+espacamento,ponto1[1],ponto1[2]-espacamento);
 
@@ -32,8 +36,40 @@ void plane(float length,int divisions, char* file){
 	fprintf(fd, "%f\n %d\n", length, divisions);
 
 }
+
+void sphere(float radius, int slices, int stacks, char* file){
+	//nrvertices
+	//spamar os vertices
+	//radius
+	//slices
+	//stacks
+	//Cartesian Coordinates
+	//𝑝𝑧 = 𝑟 × cos 𝛽 × cos(𝛼);
+	//𝑝𝑥 = 𝑟 × cos 𝛽 × sin(𝛼);
+	//𝑝𝑦 = 𝑟 × sin(𝛽);
+	FILE *fd = fopen(file,"w+");
+	int vertices = slices * stacks * 6;
+	fprintf(fd, "%d\n", vertices );
+	float lat = 2 * M_PI / slices;
+	float lon = 2 * M_PI / stacks;
+	for(int i=0;i<stacks;i++){
+		for(int j=0;j<slices;j++){
+			float currentlat = j * lat;
+			float currentlon = (-1/2) * M_PI + i * lon;
+			fprintf(fd, "%f %f %f\n",radius * cos(currentlat) * sin(currentlon),radius * sin(currentlon),radius * cos(currentlat) * cos(currentlon));
+			fprintf(fd, "%f %f %f\n",radius * cos(currentlat) * sin(currentlon),radius * sin(currentlon + lon),radius * cos(currentlat) * cos(currentlon));
+			fprintf(fd, "%f %f %f\n",radius * cos(currentlat+lat) * sin(currentlon+lon),radius * sin(currentlon + lon),radius * cos(currentlat+lat) * cos(currentlon+lon));
+
+			fprintf(fd, "%f %f %f\n",radius * cos(currentlat) * sin(currentlon),radius * sin(currentlon),radius * cos(currentlat) * cos(currentlon));
+			fprintf(fd, "%f %f %f\n",radius * cos(currentlat+lat) * sin(currentlon),radius * sin(currentlon),radius * cos(currentlat+lat) * cos(currentlon));
+			fprintf(fd, "%f %f %f\n",radius * cos(currentlat+lat) * sin(currentlon+lon),radius * sin(currentlon + lon),radius * cos(currentlat+lat) * cos(currentlon+lon));
+		}	
+	}
+	fprintf(fd, "%f\n%d\n%d\n", radius,slices,stacks);
+}
+
 int main(int argc, char** argv){
-	if(argv[1]=="plane"){
+	if(strcmp(argv[1],"plane")){
 		plane(atof(argv[2]),atoi(argv[3]), argv[4]);
 	}
 
